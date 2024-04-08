3f299a64b9011c66f85c559341d02ab05a9410de
lumpyspaceyz <yzever99@gmail.com>
2024-04-08 20:56:56 +0900

    [Fix] - 키보드 접근 시 발생하는 오류 수정

--- src/pages/user/components/Tab.tsx
+++ src/pages/user/components/Tab.tsx
@@ -8,10 +8,8 @@ type tabItemProps = {
 };
 
 const classNameMappings = {
-  active:
-    'basis-1/3 rounded-lg px-6pxr py-8pxr shadow-default bg-white hover:bg-white hover:text-gray-700',
-  inactive:
-    'basis-1/3 rounded-lg px-6pxr py-8pxr text-gray-500 hover:bg-white hover:text-gray-700 hover:shadow',
+  active: 'basis-1/3 rounded-lg px-6pxr py-8pxr shadow-default bg-white hover:bg-white hover:text-gray-700',
+  inactive: 'basis-1/3 rounded-lg px-6pxr py-8pxr text-gray-500 hover:bg-white hover:text-gray-700 hover:shadow',
 };
 
 const TabItem = ({ label, path, currentPath }: tabItemProps) => {
@@ -25,9 +23,7 @@ const TabItem = ({ label, path, currentPath }: tabItemProps) => {
   return (
     <>
       <li className={classNameMappings[isActive ? 'active' : 'inactive']}>
-        <button onClick={handleClick} onKeyDown={handleClick}>
-          {label}
-        </button>
+        <button onClick={handleClick}>{label}</button>
       </li>
     </>
   );
@@ -45,16 +41,11 @@ const Tab = () => {
 
   return (
     <>
-      <div className="mx-14pxr">
-        <div className="mt-37pxr p-6pxr w-full rounded-[12px] bg-gray_150 flex flex-row justify-center items-center">
+      <div className="mx-14pxr mt-37pxr">
+        <div className="p-6pxr w-full rounded-[12px] bg-gray_150 flex flex-row justify-center items-center">
           <ul className="flex flex-row gap-5pxr text-center text-sm text-foot w-full">
             {tabs.map((tab) => (
-              <TabItem
-                key={tab.path}
-                label={tab.label}
-                path={tab.path}
-                currentPath={currentPath}
-              />
+              <TabItem key={tab.path} label={tab.label} path={tab.path} currentPath={currentPath} />
             ))}
           </ul>
         </div>
